d9f432d950f8d4bdff0c0966aafed9a0d4cd02f8
Walter Garcia <waltermbgarcia@hotmail.com>
2020-01-21 15:16:03 -0300

    Add and configure bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap';
+
+import '../src/application.css';
